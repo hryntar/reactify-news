7b2ad24e0348890cdb14cd6bbe39af7697742760
Hrynchuk Taras <taras.hrynchuk.ri.2023@lpnu.ua>
2024-01-17 12:52:47 +0200

    added plug for non-existing image

--- src/components/NewsList.tsx
+++ src/components/NewsList.tsx
@@ -7,7 +7,7 @@ const NewsList: FC<{ news: NewsType[] }> = ({ news }) => {
       <ul className="newsList">
          {news.map((article) => (
             <li className="newsList__item item-newsList" key={article.id}>
-               <div className="item-newsList__img-ibg"><img src={article.image} alt="News image"/></div>
+               <div className="item-newsList__img-ibg">{article.image == "None" ? <div className="image-plug"></div> : <img src={article.image} alt="News image"/>}</div>
                <div className="item-newsList__body">
                   <h3 className="item-newsList__title">{article.title}</h3>
                   <p className="item-newsList__info">{formatTimeAgo(article.published)} · by {article.author}</p>
