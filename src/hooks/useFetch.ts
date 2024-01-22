f9341d5cf678e315064b130477edfd87cb3c1675
Hrynchuk Taras <taras.hrynchuk.ri.2023@lpnu.ua>
2024-01-22 15:18:31 +0200

    useFetch custom hook added, refactoring

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import { IGetNews } from "../api/apiNews";
+
+export const useFetch = <T>(
+   fetchFunction: (obj: IGetNews) => Promise<T>,
+   params?: IGetNews
+): { data: T | null; isLoading: boolean; error: unknown | null } => {
+   const [data, setData] = useState<T | null>(null);
+   const [isLoading, setIsLoading] = useState(true);
+   const [error, setError] = useState<null | unknown>(null);
+
+   const stringParams: string = params ? new URLSearchParams(params).toString() : "";
+
+   useEffect(() => {
+      (async () => {
+         try {
+            setIsLoading(true);
+            const response = await fetchFunction(params!);
+            setData(response);
+         } catch (error) {
+            setError(error);
+         } finally {
+            setIsLoading(false);
+         }
+      })();
+   }, [fetchFunction, stringParams]);
+
+   return { data, isLoading, error };
+};
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,69 +1,39 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import NewsBanner from "../components/NewsBanner/NewsBanner";
-import { getCategories, getNews } from "../api/apiNews";
-import { NewsType } from "../types/NewsType";
+import { CategoriesResponse, NewsResponse, getCategories, getNews } from "../api/apiNews";
 import NewsList from "../components/NewsList/NewsList";
 import Pagination from "../components/UI/Pagination/Pagination";
 import Categories from "../components/Categories/Categories";
 import Search from "../components/UI/Search/Search";
 import { useDebounce } from "../hooks/useDebounce";
 import { PAGE_SIZE, TOTAL_PAGES } from "../constants/constants";
+import { useFetch } from "../hooks/useFetch";
 
-const Main = () => {
-   const [news, setNews] = useState<NewsType[]>();
-   const [categories, setCategories] = useState<string[]>();
-   const [isLoading, setIsLoading] = useState(true);
+const Main = () => { 
    const [currentPage, setCurrentPage] = useState(1);
    const [currentCategory, setCurrentCategory] = useState(0);
    const [keywords, setKeywords] = useState("");
-  
 
    const debouncedKeywords = useDebounce(keywords, 1000);
 
-   const fetchNews = async (currentPage: number, currentCategory: number, categories: string[], debouncedKeywords: string) => {
-      try {
-         setIsLoading(true);
-         const data: { status: string; news: NewsType[]; page: number } = await getNews({
-            page_number: currentPage,
-            page_size: PAGE_SIZE,
-            category: currentCategory === 0 ? "all" : categories[currentCategory],
-            keywords: debouncedKeywords,
-         });
-         console.log(data.news);
-         setNews(data.news);
-         setIsLoading(false);
-      } catch (error) {
-         console.error(error);
-      }
-   };
+   const { data: dataCategories} = useFetch<CategoriesResponse>(getCategories);
 
-   const fetchCategories = async () => {
-      try {
-         const data: { categories: string[]; description: string; status: string } = await getCategories();
-         console.log(data.categories);
-         setCategories(["all", ...data.categories]);
-      } catch (error) {
-         console.error(error);
-      }
-   };
-
-   useEffect(() => {
-      fetchCategories();
-   }, []);
-
-   useEffect(() => {
-      fetchNews(currentPage, currentCategory, categories ? categories : [], debouncedKeywords);
-   }, [currentPage, currentCategory, categories, debouncedKeywords]);
+   const { data, isLoading } = useFetch<NewsResponse>(getNews, {
+      page_number: currentPage,
+      page_size: PAGE_SIZE,
+      category: currentCategory === 0 ? "all" : dataCategories ? dataCategories?.categories[currentCategory - 1] : '',
+      keywords: debouncedKeywords,
+   }); 
 
    return (
       <main className="main">
          <div className="main__container">
-            {categories?.length ? (
-               <Categories categories={categories} currentCategory={currentCategory} setCurrentCategory={setCurrentCategory} />
+            {dataCategories ? (
+               <Categories categories={["all", ...dataCategories.categories]} currentCategory={currentCategory} setCurrentCategory={setCurrentCategory} />
             ) : null}
             <Search keywords={keywords} setKeywords={setKeywords} />
-            <NewsBanner isLoading={isLoading} news={news && news.length > 0 ? news[0] : null} />
-            <NewsList isLoading={isLoading} news={news && news.length > 0 ? news : null} /> 
+            <NewsBanner isLoading={isLoading} news={data && data.news.length > 0 ? data.news[0] : null} />
+            <NewsList isLoading={isLoading} news={data && data.news.length > 0 ? data.news : null} />
          </div>
          <Pagination totalPages={TOTAL_PAGES} setCurrentPage={setCurrentPage} currentPage={currentPage} />
       </main>
